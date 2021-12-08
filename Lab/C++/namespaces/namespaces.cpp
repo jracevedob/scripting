a37bc0f588545ac43f8c652fc5fcb0ae221e7729
jracevedob <jracevedob@gmail.com>
2021-12-08 21:41:46 +0100

    Pointers exercises

--- Lab/C++/namespaces/namespaces.cpp
+++ Lab/C++/namespaces/namespaces.cpp
@@ -18,7 +18,11 @@
  #define DIMARRAYX 10
  #define DIMARRAYY 10
  
+ /*I/O functions cout, cin objects*/
  #include <iostream>
+ /*sran funciton*/
+ #include <cstdlib>
+ /* readout and write files */
  #include <sstream>
  #include <iomanip>
  #include <string>
@@ -37,8 +41,58 @@
  	return(x>y) ? x : y;
  }
  
+ struct productCharacteristic {
+ 	int weight;
+ 	double priceEuro;
+ 	double priceDollar;
+ };
+ 
  
  /* Function in C++*/
+ void print1DArray(uint16_t arg[], int length)
+ {
+ 	std::cout << "Print array: " << std::endl;
+ 	for(int n=0 ; n<length ; n++)
+ 		std::cout << arg[n] << " ";
+ 	std::cout << "\n";
+ }
+ 
+ void print2DArray(uint16_t arg[], int length)
+ {
+ 	/* Auxiliar variables */
+ 	int n,m = 0;
+ 	
+ 	std::cout << "Print 2D Array: " << std::endl;
+ 	for(n = 0 ; n<DIMARRAYX ; n++){
+ 		for(m = 0; m<DIMARRAYY ; m++){
+ 			std::cout << arg[n*DIMARRAYY+m] << " ";
+ 				if(m == DIMARRAYY-1)
+ 					std::cout<< " \n"; 	
+ 		}
+ 	}
+ }
+ 
+ /* Data structures */
+ 
+ void incrementValue(int *start, int *stop)
+ {
+ 	int *current = start;
+ 	while(current != stop){
+ 		++(*current);
+		current++; 	
+ 	}
+ }
+ 
+ void printValues(const int *start, const int *stop)
+ {
+ 	const int *current = start;
+ 	while(current != stop){
+ 		std::cout<<*current<<'\n';
+ 		++current;
+ 	}
+ }
+ 
+ 
 
  
  int main(int argc, char** argv)
@@ -60,17 +114,57 @@
  	
  	// Multidimentional array declaration
  	
- 	uint16_t arrayMultiDim[DIMARRAYX][DIMARRAYY];
+ 	uint16_t arrayMultiDim[DIMARRAYX*DIMARRAYY];
  	
- 	for(i=0 ; i<DIMARRAYX ; i++){
+ 	/*for(i=0 ; i<DIMARRAYX ; i++){
  		for(j=0 ; j<DIMARRAYY ; j++){
- 			arrayMultiDim[i][j] = i*j*2+j;
- 			std::cout << " " << arrayMultiDim[i][j]; 
+ 			arrayMultiDim[i*DIMARRAYY+j] = i*j*2+j;
+ 			std::cout << " " << arrayMultiDim[i*DIMARRAYY+j]; 
  			if (j == DIMARRAYY-1)
  				std::cout << " \n" << std::endl; 	
  		}
- 	}
+ 	} */
  	
+ 	/* Print funcitons */
+ 	//print1DArray(arrayMultiDim, DIMARRAYY*DIMARRAYX);
+ 	//print2DArray(arrayMultiDim, DIMARRAYY*DIMARRAYX);
+ 	
+ 	/* Pointers and data structures */
+ 	
+ 	int firstValue, secondValue;
+ 	int *myPnt;
+ 	
+ 	myPnt = &firstValue;
+ 	*myPnt = 10;
+ 	myPnt = &secondValue;
+ 	*myPnt = 20;
+ 	
+ 	std::cout << "First Value: " << firstValue << " Second Value: " << secondValue << std::endl;
+ 	
+ 	/* Pointers and arrays */
+ 	
+ 	int numbers[5];
+ 	int *pnt;
+ 	
+ 	pnt = numbers;
+ 	
+ 	for(i=0 ; i<5 ; i++)
+ 		*(pnt+i) = i*2;
+ 	
+ 	for(j=0 ; j<5 ; j++)
+ 		{std::cout<<numbers[j]<<std::endl;}
+ 		
+ 	/* Pointer arithmetics */
+ 	
+ 	int valuesNumeric[] ={10,20,30,40};
+ 	//incrementValue(valuesNumeric, valuesNumeric+3);
+ 	printValues(valuesNumeric, valuesNumeric);
+ 	
+ 	j= 0;
+ 	while (j != 2){
+ 		cout<<j<<endl; 
+ 		j++;
+ 	}
  	
  	
  
