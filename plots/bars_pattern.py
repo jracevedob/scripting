28af119b74273b3e1f0c2856e613c2afae414f5a
jracevedob <jracevedob@gmail.com>
2022-03-04 02:50:28 +0100

    Template plots

---
+++ plots/bars_pattern.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+######################################################################################
+#
+# Template for plot generations: Bars with pattern
+#
+# 2022, jacevedo - TUD Telekom Chair for Communication Networks
+#
+######################################################################################
+
+
+
